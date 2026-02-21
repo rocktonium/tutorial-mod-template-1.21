fe15330d54a191efc84de76b17377b7d1d7e3e73
rocktonium <devnihalani120@gmail.com>
2026-02-20 20:51:41 -0500

    2 new items

--- src/main/java/dvsns/tutorialmod/TutorialMod.java
+++ src/main/java/dvsns/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package dvsns.tutorialmod;
 
+import dvsns.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -13,10 +14,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/dvsns/tutorialmod/item/ModItems.java
@@ -0,0 +1,30 @@
+package dvsns.tutorialmod.item;
+
+import dvsns.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+public static final Item JADE_GEM = registerItem("jade_gem", new Item(new Item.Settings()));
+public static final Item CRYSTALLINE_JADE = registerItem("crystalline_jade", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(JADE_GEM);
+            fabricItemGroupEntries.add(CRYSTALLINE_JADE);
+        });
+    }
+}
